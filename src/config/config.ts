535b4316f587731969e2eaad3c4bab43220231da
Sawyer Cutler <sawyer.cutler@gmail.com>
2022-06-08 11:17:12 -0700

    MYL8.5.2 Added Port and Listener to Run Faucet

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,2 +1,3 @@
 export const PRIVATE_KEY: string = process.env.PRIVATE_KEY as string;
 export const WS_URL: string = process.env.WS_URL as string;
+export const PORT: number = Number(process.env.PORT);
