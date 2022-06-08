13adc792050f7e30cc5f748cea60a291c30a8bb9
Sawyer Cutler <sawyer.cutler@gmail.com>
2022-06-08 12:42:33 -0700

    Added DOT Name Service to Deployment

--- src/config/addresses.ts
+++ src/config/addresses.ts
@@ -3,6 +3,6 @@ type IAddress = {
 }
 
 export const ADDRESSES: IAddress = {
-    "dot_name_service": "",
+    "dot_name_service": "0x3D65d8525659640d0Ad36B283EdbEF6975af7520",
     "etherbase": "0xd2bA3e0000000000000000000000000000000000"
 };
