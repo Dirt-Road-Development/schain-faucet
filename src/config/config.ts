d7d4da7253a4e1b3092948fa102397038c7b8bb3
Sawyer Cutler <sawyer.cutler@gmail.com>
2022-06-09 14:56:39 -0700

    Hot Fix for Env Issues

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config()
+
 export const PRIVATE_KEY: string = process.env.PRIVATE_KEY as string;
 export const WS_URL: string = process.env.WS_URL as string;
 export const PORT: number = Number(process.env.PORT);
